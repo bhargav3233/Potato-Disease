9f92dec65c2847de32d00bb27e48308bf2fb913f
bhargav3233 <109935418+bhargav3233@users.noreply.github.com>
2022-08-27 03:28:12 +0530

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -34,7 +34,7 @@ def predict_class(image) :
         classifier_model = keras.models.load_model(r'final_model.h5', compile = False)
 
     shape = ((256,256,3))
-    model = keras.Sequential([hub.KerasLayer(classifier_model, input_shape = shape)])     # ye bhi kaam kar raha he
+    model = keras.Sequential([hub.KerasLayer(classifier_model, input_shape = shape)])   
     test_image = image.resize((256, 256))
     test_image = keras.preprocessing.image.img_to_array(test_image)
     test_image /= 255.0
@@ -71,7 +71,7 @@ a:hover,  a:active {
 </style>
 
 <div class="footer">
-<p align="center"> <a href="https://www.linkedin.com/in/ronylpatil/">Developed with ❤ by ronil</a></p>
+<p align="center"> <a href="https://www.linkedin.com/in/bhargav-reddy-8602b1221/">Developed with ❤ by Bhargav</a></p>
 </div>
         """
 
